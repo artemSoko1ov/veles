6ae25c0ee456cb3392c1939fbe530e7088e3ab21
artem <the.jelll.2006@gmail.com>
2026-08-05 20:08:51 +0500

    feat: add Header markup

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,42 @@
+import styles from './Header.module.scss';
+import clsx from "clsx";
+import {navItems} from "@/widgets/Header/model/navItems";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={clsx(styles.inner, 'container')}>
+        <a
+          className={styles.logo}
+          href="#"
+          aria-label="На главную"
+        >
+          Logo
+        </a>
+        <nav aria-label="Основная навигация">
+          <ul className={styles.list}>
+            {navItems.map(({name, link}) => (
+              <li
+                className={styles.item}
+                key={link}
+              >
+                <a
+                  className={styles.link}
+                  href={link}
+                >
+                  {name}
+                </a>
+
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.phone}>
+          <a href="tel:89999999999">8(9XX)-XXX-XX-XX</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
