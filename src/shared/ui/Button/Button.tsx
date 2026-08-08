ad14c2e9499c20721dbb08364c0a44896acc5f37
artem <the.jelll.2006@gmail.com>
2026-08-08 12:49:36 +0500

    feat: add Button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,33 @@
+import styles from './Button.module.scss';
+import type {ButtonHTMLAttributes, PropsWithChildren} from "react";
+import clsx from "clsx";
+
+type Variant = "primary" | "outline";
+
+type Props = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>> & {
+  variant?: Variant;
+};
+
+const Button = ({
+                  className,
+                  children,
+                  type = "button",
+                  variant = "primary",
+                  ...props
+                }: Props) => {
+  return (
+    <button
+      className={clsx(
+        styles.button,
+        styles[variant],
+        className
+      )}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,9 +1,17 @@
 import styles from './Header.module.scss';
 import clsx from "clsx";
-import { navItems } from "@/widgets/Header/model/navItems";
+import {navItems} from "@/widgets/Header/model/navItems";
 import Logo from "@/shared/ui/Logo";
+import {useState} from "react";
+import Button from "@/shared/ui/Button";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <header className={styles.header}>
       <div className={clsx(styles.inner, 'container')}>
@@ -19,7 +27,7 @@ const Header = () => {
           aria-label="Основная навигация"
         >
           <ul className={styles.list}>
-            {navItems.map(({ id, name, link }) => (
+            {navItems.map(({id, name, link}) => (
               <li
                 className={styles.item}
                 key={id}
@@ -37,6 +45,13 @@ const Header = () => {
         <div className={styles.phone}>
           <a href="tel:89999999999">8(9XX)-XXX-XX-XX</a>
         </div>
+
+        <Button
+          variant="outline"
+          onClick={toggleMenu}
+        >
+          Бургер меню
+        </Button>
       </div>
     </header>
   );
