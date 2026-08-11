6cb2a3a3cee7280bb68c5f8934626cb484829419
artem <the.jelll.2006@gmail.com>
2026-08-11 19:42:06 +0500

    feat: add markup Section

--- src/shared/ui/Section/Section.tsx
+++ src/shared/ui/Section/Section.tsx
@@ -1,9 +1,35 @@
 import styles from './Section.module.scss';
+import clsx from "clsx";
+import type {HTMLAttributes, PropsWithChildren} from "react";
 
-const Section = () => {
+type Props = PropsWithChildren<HTMLAttributes<HTMLElement>> & {
+  title: string;
+  description?: string;
+};
+
+const Section = ({
+                   title,
+                   description,
+                   className,
+                   id,
+                   children,
+                   ...props
+                 }: Props) => {
   return (
-    <section className={styles.section}>
-
+    <section
+      className={clsx(styles.section, className)}
+      id={id}
+      {...props}
+    >
+      <div className={clsx(styles.inner, "container")}>
+        <div className={styles.head}>
+          <h2 className={styles.title}>{title}</h2>
+          {description && <p className={styles.description}>{description}</p>}
+        </div>
+        <div className={styles.body}>
+          {children}
+        </div>
+      </div>
     </section>
   );
 };
