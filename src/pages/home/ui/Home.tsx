72c2517b91456786f7db071aca64b2322fe44a61
artem <the.jelll.2006@gmail.com>
2026-08-19 22:26:23 +0500

    feat: create Advantages section

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/widgets/Hero";
+import Advantages from "@/widgets/Advantages";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Advantages />
     </>
   );
 };
---
+++ src/widgets/Advantages/ui/Advantages.tsx
@@ -0,0 +1,11 @@
+import styles from './Advantages.module.scss';
+
+const Advantages = () => {
+  return (
+    <section className={styles.advantages}>
+      Advantages
+    </section>
+  );
+};
+
+export default Advantages;
