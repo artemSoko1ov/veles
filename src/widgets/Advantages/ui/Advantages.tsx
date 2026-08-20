bf5b63760459cc79cf5aeabd1616443260180bd1
artem <the.jelll.2006@gmail.com>
2026-08-20 12:43:10 +0500

    feat: add content in Advantages

--- src/widgets/Advantages/ui/Advantages.tsx
+++ src/widgets/Advantages/ui/Advantages.tsx
@@ -1,5 +1,7 @@
 import styles from './Advantages.module.scss';
 import Section from "@/shared/ui/Section";
+import Card from "@/shared/ui/Card";
+import images from "@/shared/assets/images/cards/coffee.png"
 
 const Advantages = () => {
   return (
@@ -8,7 +10,43 @@ const Advantages = () => {
       description="От семейного ужина до большого банкета — создаем комфортную атмосферу, готовим домашние блюда и заботимся о каждом госте."
       className={styles.advantages}
     >
-      Advantages
+      <div className={styles.grid}>
+        <Card
+          image={images}
+          title="Домашняя кухня"
+          description="Русские и европейские блюда, которые готовим после заказа из свежих продуктов."
+        />
+
+        <Card
+          image={images}
+          title="Домашняя кухня"
+          description="Русские и европейские блюда, которые готовим после заказа из свежих продуктов."
+        />
+
+        <Card
+          image={images}
+          title="Бизнес-ланчи"
+          description="Сытный обед по будням с 12:00 до 16:00."
+        />
+
+        <Card
+          image={images}
+          title="Банкеты и мероприятия"
+          description="Юбилеи, корпоративы, свадьбы и другие праздники в уютной атмосфере."
+        />
+
+        <Card
+          image={images}
+          title="Уютная атмосфера"
+          description="Спокойное место для семейного обеда, встречи или вечера с близкими."
+        />
+
+        <Card
+          image={images}
+          title="Кофе с собой"
+          description="Хороший кофе, который можно взять с собой."
+        />
+      </div>
     </Section>
   );
 };
