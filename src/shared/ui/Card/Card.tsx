79ec03e74afe57a1dde75e96476fc03f50a646ec
artem <the.jelll.2006@gmail.com>
2026-08-20 11:27:00 +0500

    feat: add markup and style in Card component

---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,41 @@
+import styles from './Card.module.scss';
+import type {HTMLAttributes, PropsWithChildren} from "react";
+import clsx from "clsx";
+
+type Props = PropsWithChildren<HTMLAttributes<HTMLElement>> & {
+  title: string;
+  description?: string;
+  image: string;
+
+};
+
+const Card = ({
+                title,
+                description,
+                image,
+                className,
+                children,
+                ...props
+              }: Props) => {
+  return (
+    <div className={clsx(styles.card, className)} {...props}>
+      <img
+        className={styles.image}
+        src={image}
+        alt=""
+      />
+
+      {(title || description) && (
+        <div className={styles.overlay}>
+          {title && <h3 className={styles.title}>{title}</h3>}
+
+          {description && (
+            <p className={styles.description}>{description}</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Card;
