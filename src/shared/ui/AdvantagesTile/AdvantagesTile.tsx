c2b547b2a7c64a57cb7b951762d80bceabceb7f4
artem <the.jelll.2006@gmail.com>
2026-08-20 10:48:42 +0500

    feat: create Card component

---
+++ src/shared/ui/AdvantagesTile/AdvantagesTile.tsx
@@ -0,0 +1,11 @@
+import styles from './AdvantagesTile.module.scss';
+
+const AdvantagesTile = () => {
+  return (
+    <div className={styles.advantagesTile}>
+
+    </div>
+  );
+};
+
+export default AdvantagesTile;
