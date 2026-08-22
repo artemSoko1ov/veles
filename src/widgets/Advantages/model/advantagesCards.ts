85e0a72d6060a4503c6ab26c02884b8c9a8273f8
artem <the.jelll.2006@gmail.com>
2026-08-22 14:51:36 +0500

    style: update Advantages images

--- src/widgets/Advantages/model/advantagesCards.ts
+++ src/widgets/Advantages/model/advantagesCards.ts
@@ -1,34 +1,39 @@
-import coffeeImage from "@/shared/assets/images/cards/coffee.png"
+import cardImage1 from "@/shared/assets/images/cards/1.jpg";
+import cardImage2 from "@/shared/assets/images/cards/2.jpg";
+import cardImage3 from "@/shared/assets/images/cards/3.jpg";
+import cardImage4 from "@/shared/assets/images/cards/4.jpg";
+import cardImage5 from "@/shared/assets/images/cards/5.jpg";
+import cardImage6 from "@/shared/assets/images/cards/6.jpg";
 
 export const advantagesCards = [
   {
     title: 'Для любых событий',
     description: 'От семейного ужина до свадьбы или корпоратива — поможем создать подходящую атмосферу для вашего праздника.',
-    image: coffeeImage,
+    image: cardImage1,
   },
   {
     title: 'Домашняя кухня',
     description: 'Русские и европейские блюда, которые готовим после заказа из свежих продуктов.',
-    image: coffeeImage,
+    image: cardImage2,
   },
   {
     title: 'Бизнес-ланчи',
-    description: 'Сытный обед по будням с 12:00 до 16:00..',
-    image: coffeeImage,
+    description: 'Сытный обед по будням с 12:00 до 16:00.',
+    image: cardImage3,
   },
   {
     title: 'Банкеты и мероприятия',
     description: 'Юбилеи, корпоративы, свадьбы и другие праздники в уютной атмосфере.',
-    image: coffeeImage,
+    image: cardImage4,
   },
   {
     title: 'Уютная атмосфера',
     description: 'Спокойное место для семейного обеда, встречи или вечера с близкими.',
-    image: coffeeImage,
+    image: cardImage5,
   },
   {
     title: 'Кофе с собой',
     description: 'Хороший кофе, который можно взять с собой.',
-    image: coffeeImage,
+    image: cardImage6,
   },
 ]
