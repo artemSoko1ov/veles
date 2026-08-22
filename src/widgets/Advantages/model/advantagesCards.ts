c45de1cab1f025b02aed0931b19ae16b26767edd
artem <the.jelll.2006@gmail.com>
2026-08-22 14:16:53 +0500

    refactor: extract card data

---
+++ src/widgets/Advantages/model/advantagesCards.ts
@@ -0,0 +1,34 @@
+import coffeeImage from "@/shared/assets/images/cards/coffee.png"
+
+export const advantagesCards = [
+  {
+    title: 'Для любых событий',
+    description: 'От семейного ужина до свадьбы или корпоратива — поможем создать подходящую атмосферу для вашего праздника.',
+    image: coffeeImage,
+  },
+  {
+    title: 'Домашняя кухня',
+    description: 'Русские и европейские блюда, которые готовим после заказа из свежих продуктов.',
+    image: coffeeImage,
+  },
+  {
+    title: 'Бизнес-ланчи',
+    description: 'Сытный обед по будням с 12:00 до 16:00..',
+    image: coffeeImage,
+  },
+  {
+    title: 'Банкеты и мероприятия',
+    description: 'Юбилеи, корпоративы, свадьбы и другие праздники в уютной атмосфере.',
+    image: coffeeImage,
+  },
+  {
+    title: 'Уютная атмосфера',
+    description: 'Спокойное место для семейного обеда, встречи или вечера с близкими.',
+    image: coffeeImage,
+  },
+  {
+    title: 'Кофе с собой',
+    description: 'Хороший кофе, который можно взять с собой.',
+    image: coffeeImage,
+  },
+]
--- src/widgets/Advantages/ui/Advantages.tsx
+++ src/widgets/Advantages/ui/Advantages.tsx
@@ -1,7 +1,7 @@
 import styles from './Advantages.module.scss';
 import Section from "@/shared/ui/Section";
 import Card from "@/shared/ui/Card";
-import images from "@/shared/assets/images/cards/coffee.png"
+import {advantagesCards} from "@/widgets/Advantages/model/advantagesCards.ts";
 
 const Advantages = () => {
   return (
@@ -11,41 +11,14 @@ const Advantages = () => {
       className={styles.advantages}
     >
       <div className={styles.grid}>
-        <Card
-          image={images}
-          title="Домашняя кухня"
-          description="Русские и европейские блюда, которые готовим после заказа из свежих продуктов."
-        />
-
-        <Card
-          image={images}
-          title="Домашняя кухня"
-          description="Русские и европейские блюда, которые готовим после заказа из свежих продуктов."
-        />
-
-        <Card
-          image={images}
-          title="Бизнес-ланчи"
-          description="Сытный обед по будням с 12:00 до 16:00."
-        />
-
-        <Card
-          image={images}
-          title="Банкеты и мероприятия"
-          description="Юбилеи, корпоративы, свадьбы и другие праздники в уютной атмосфере."
-        />
-
-        <Card
-          image={images}
-          title="Уютная атмосфера"
-          description="Спокойное место для семейного обеда, встречи или вечера с близкими."
-        />
-
-        <Card
-          image={images}
-          title="Кофе с собой"
-          description="Хороший кофе, который можно взять с собой."
-        />
+        {advantagesCards.map(({title, description, image}) => (
+          <Card
+            title={title}
+            description={description}
+            image={image}
+            key={title}
+          />
+        ))}
       </div>
     </Section>
   );
