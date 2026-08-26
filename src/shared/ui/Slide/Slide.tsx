295c06b9c882276e9eaf68f1c8bdcfedc1210778
artem <the.jelll.2006@gmail.com>
2026-08-26 12:21:28 +0500

    feat: add markup in slide component

--- src/shared/ui/Slide/Slide.tsx
+++ src/shared/ui/Slide/Slide.tsx
@@ -1,9 +1,24 @@
 import styles from './Slide.module.scss'
 
-const Slide = () => {
+type Props = {
+  title: string;
+  description: string;
+  image: string;
+};
+
+const Slide = ({title, description, image}: Props) => {
   return (
     <div className={styles.slide}>
-
+      <img
+        className={styles.image}
+        src={image}
+        alt=""
+        loading="lazy"
+      />
+      <div className={styles.body}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
