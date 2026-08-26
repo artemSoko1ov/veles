2f1d7e162dcfd47db4f1ad4f559ef6c079b63b5c
artem <the.jelll.2006@gmail.com>
2026-08-26 20:35:52 +0500

    feat: create files Contacts section

---
+++ src/widgets/Contacts/ui/Contacts.tsx
@@ -0,0 +1,11 @@
+import styles from './Contacts.module.scss';
+
+const Contacts = () => {
+  return (
+    <section className={styles.contacts}>
+
+    </section>
+  );
+};
+
+export default Contacts;
