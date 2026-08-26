9605b33e9c0399735d7aa97c80e06a2cb5051a51
artem <the.jelll.2006@gmail.com>
2026-08-26 12:12:28 +0500

    feat: add Kitchen slider data

---
+++ src/widgets/Kitchen/model/kitchenSlides.ts
@@ -0,0 +1,19 @@
+import steakImage from "@/shared/assets/images/kitchen/1.jpg";
+
+export const kitchenSlides = [
+  {
+    title: "Горячие блюда",
+    description: "Сытные блюда из мяса, птицы и рыбы с гарниром.",
+    image: steakImage,
+  },
+  {
+    title: "Свежие салаты",
+    description: "Лёгкие салаты из свежих овощей и качественных продуктов.",
+    image: steakImage,
+  },
+  {
+    title: "Домашние супы",
+    description: "Тёплые и сытные супы для полноценного обеда.",
+    image: steakImage,
+  },
+];
